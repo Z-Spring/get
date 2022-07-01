c580ea5a1d1e8140f9821761d49948496a6b3c7e
murphy <murphyqq1@gmail.com>
2022-07-02 02:20:46 +0800

    fix: add redis password and use redis-string

--- myredis/redis.go
+++ myredis/redis.go
@@ -2,20 +2,34 @@ package myredis
 
 import (
 	"context"
-	"fmt"
+	"errors"
 	"github.com/go-redis/redis/v9"
 	"log"
+	"time"
 )
+
+func init() {
+	log.SetFlags(log.Ldate | log.Ltime | log.Lshortfile)
+}
 
 func NewRedis() *redis.Client {
 	rdb := redis.NewClient(&redis.Options{
-		Addr: "101.42.224.110:6379",
-		DB:   0,
+		Addr:     "101.42.224.110:6379",
+		Password: "ubuntu",
+		DB:       0,
 	})
 	return rdb
 }
 
-func AddPkgToRedis(pkgName string, pkgRepo string) {
+// AddNameToRedis add name and pkg to redis-string
+func AddNameToRedis(pkgName string, pkgRepo string) {
+	client := NewRedis()
+
+	client.Set(context.Background(), pkgName, pkgRepo, 100*time.Hour)
+}
+
+// AddNameToRedis2 add name to redis-list
+func AddNameToRedis2(pkgName string) {
 	client := NewRedis()
 
 	n := client.LPush(context.Background(), "pkgNames", pkgName)
@@ -23,23 +37,29 @@ func AddPkgToRedis(pkgName string, pkgRepo string) {
 	if err != nil {
 		log.Panicln(err)
 	}
-
-	r := client.LPush(context.Background(), "pkgRepos", pkgRepo)
-	_, err2 := r.Result()
-	if err != nil {
-		log.Panicln(err2)
-	}
-	fmt.Println("done")
 }
 
-func GetPkgNameFromRedis() []string {
+// GetPkg get name from key[pkgName],it uses redis-string
+func GetPkg(pkgName string) (string, error) {
+	client := NewRedis()
+	result := client.Get(context.Background(), pkgName)
+	pkgRepo, err := result.Result()
+	if err != nil {
+		error := errors.New("can't find this pkgName")
+		return "", error
+	}
+	return pkgRepo, nil
+}
+
+// GetNamesFromRedis get all names from key[pkgNames],it uses redis-list
+func GetNamesFromRedis() []string {
 	client := NewRedis()
 	result := client.LRange(context.Background(), "pkgNames", 0, -1)
 	vals := result.Val()
 	return vals
 }
 
-func GetPkgRepoFromRedis() []string {
+func GetReposFromRedis() []string {
 	client := NewRedis()
 	result := client.LRange(context.Background(), "pkgRepos", 0, -1)
 	vals := result.Val()
