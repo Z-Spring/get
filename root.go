58cab5cdd5314713573e6cd005745726eafc96d9
murphy <murphyqq1@gmail.com>
2022-07-02 02:24:02 +0800

    fix: modify command long descriptions and delete some unused codes

--- root.go
+++ root.go
@@ -5,8 +5,9 @@ Copyright © 2022 murphy <murphyqq1@gmail.com>
 package main
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
-	"log"
+	"os"
 )
 
 // rootCmd represents the base command when called without any subcommands
@@ -14,28 +15,19 @@ var rootCmd = &cobra.Command{
 	Use:   "get",
 	Short: "you can get go packages easily",
 	Long: `Have you ever had the problem that when you want to import a dependency package for your project, 
-you only remember its name, but not its full path? 
-For example: you want to import the gin package, 
-but you don't remember the full path to import the package (github.com/gin-gonic/gin). 
-Now you can use this tool to get the package you want more easily by typing a simple statement: [get gin]. 
+but you only remember its name, but not its full path? 
+For example: you want to get the gin package,but you don't remember its full path (github.com/gin-gonic/gin). 
+Now you can use this tool to get the package you want easier by input a simple command: [get gin]. 
 It's that simple!`,
 }
 
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		log.Fatal(err)
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
+
 }
 
 func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.get.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	//rootCmd.Flags().String("gin", "$ go get -u github.com/gin-gonic/gin", "Help message for toggle")
 }
