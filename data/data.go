42d8957885d19ef5105b81ab03272d980387584a
murphy <murphyqq1@gmail.com>
2023-04-28 11:24:47 +0800

    feat: add redis cache

--- data/data.go
+++ data/data.go
@@ -1,5 +1,23 @@
 package data
 
-func GetData() string {
-	return "data"
+import (
+	"context"
+	"time"
+
+	"github.com/go-redis/redis/v9"
+)
+
+func SetRedis() *redis.Client {
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "127.0.0.1:8080",
+		DB:   0,
+	})
+
+	return rdb
+}
+
+func AddDataToRedis(key string, value string) {
+	client := SetRedis()
+	client.Set(context.Background(), key, value, 24 * time.Hour)
+
 }
