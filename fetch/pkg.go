c8d3fabe940abb63ababd26531bb042b97649183
murphy <murphyqq1@gmail.com>
2023-12-22 13:45:23 +0800

    feat: add request timeout fix: change goquery find selector

--- fetch/pkg.go
+++ fetch/pkg.go
@@ -3,12 +3,14 @@ package fetch
 import (
 	"errors"
 	"fmt"
-	"github.com/PuerkitoBio/goquery"
 	"io"
 	"log"
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
 type Pkg struct {
@@ -22,14 +24,17 @@ func getPkgContent(keyWord string) string {
 	url := fmt.Sprintf("https://pkg.go.dev/search?q=%s", keyWord)
 
 	req, err := http.NewRequest(http.MethodGet, url, nil)
-	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005.63 Safari/537.36")
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36")
 
 	if err != nil {
 		log.Println(err)
 	}
-	resp, err := http.DefaultClient.Do(req)
+	client := http.Client{
+		Timeout: 5 * time.Second,
+	}
+	resp, err := client.Do(req)
 	if err != nil {
-		log.Println(err)
+		log.Println("Request timed out or other error occurred:", err)
 	}
 
 	bytes, err := io.ReadAll(resp.Body)
@@ -40,6 +45,8 @@ func getPkgContent(keyWord string) string {
 	return string(bytes)
 }
 
+// GetFirstPkgInfo get first pkg info
+// if you can't find the pkg, it may be this website(https://pkg.go.dev) change it structure
 func GetFirstPkgInfo(keyWord string) Pkg {
 	content := getPkgContent(keyWord)
 
@@ -49,7 +56,7 @@ func GetFirstPkgInfo(keyWord string) Pkg {
 		fmt.Println(err)
 	}
 
-	sum := doc.Find("div.SearchResults-summary > h1 > strong").Text()
+	sum := doc.Find("div.SearchResults-summary > strong").Text()
 	modules, _ := strconv.Atoi(sum)
 	if modules == 0 {
 		return Pkg{}
@@ -65,7 +72,6 @@ func GetFirstPkgInfo(keyWord string) Pkg {
 
 	fullName := selection.Find("div.SearchSnippet-headerContainer > h2 > a > span").Text()
 	fullName = strings.Trim(fullName, "()")
-
 	synopsis := selection.Find("p").Text()
 	synopsis = strings.TrimSpace(synopsis)
 
@@ -82,16 +88,17 @@ func GetFirstPkgInfo(keyWord string) Pkg {
 
 }
 
+// GetAllPkgInfos get all pkgs info
+// if you can't find the pkg, it may be this website(https://pkg.go.dev) change it structure
 func GetAllPkgInfos(keyWord string) []Pkg {
 	content := getPkgContent(keyWord)
-
 	doc, err := goquery.NewDocumentFromReader(strings.NewReader(content))
 	if err != nil {
 		err = errors.New("can't parse the fetch content")
 		fmt.Println(err.Error())
 	}
 
-	sum := doc.Find("div.SearchResults-summary > h1 > strong").Text()
+	sum := doc.Find("div.SearchResults-summary > strong").Text()
 	modules, _ := strconv.Atoi(sum)
 	if modules == 0 {
 		return nil
@@ -103,7 +110,6 @@ func GetAllPkgInfos(keyWord string) []Pkg {
 		shortName = strings.Split(shortName, "\n")[1]
 		shortName = strings.ReplaceAll(shortName, "\n", "")
 		shortName = strings.TrimSpace(shortName)
-
 		fullName := selection.Find("div.SearchSnippet-headerContainer > h2 > a > span").Text()
 		fullName = strings.Trim(fullName, "()")
 
